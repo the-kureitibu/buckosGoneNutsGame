577398565fc6ec044225573cf0da21bcd6f305eb
Joshua Alcober <joshuaalcober2019@gmail.com>
2025-06-29 02:53:50 +0800

    Added New Weapon, Implemented Rage

--- projectiles/ami_embrace.gd
+++ projectiles/ami_embrace.gd
@@ -1,16 +1,19 @@
 extends Area2D
 
-var direction: Vector2 = Vector2.UP
-var speed = 200
+#var direction: Vector2 = Vector2.UP
+#var speed = 200
 
 func _ready() -> void:
-	$AnimationPlayer.play("hanger_projectile")
+	
+	$AnimationPlayer.play("ami_embrace")
 	var tween = create_tween()
-	tween.tween_property($Sprite2D,"scale", Vector2(1.5,1.5), 0.5)
-
-func _process(delta: float) -> void:
-	position += direction * speed * delta
-
+	tween.set_parallel(true)
+	tween.tween_property($Sprite2D,"scale", Vector2(2.5,2.5), 1)
+	tween.tween_property($CollisionShape2D,"scale", Vector2(2.5,2.5), 1)
+	$QFreeTimer.start()
+	
+func _process(_delta: float) -> void:
+	#position += direction * speed * delta
 # Get better code for range, mainly setting a travelled distance from character 
 	if Globals.outside_range == true:
 		queue_free()
@@ -39,3 +42,7 @@ func _on_body_entered(body: Node2D) -> void:
 		#queue_free()
 	#else:
 	queue_free()
+
+
+func _on_q_free_timer_timeout() -> void:
+	queue_free() # Replace with function body.
--- scenes/Scenes/level.gd
+++ scenes/Scenes/level.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var get_hanger_proj: PackedScene = preload("res://projectiles/projectile_hanger.tscn")
 var get_exchu_proj: PackedScene = preload("res://projectiles/exchu_li_bladder.tscn")
-
+var get_embrace_proj: PackedScene = preload("res://projectiles/ami_embrace.tscn")
 
 func get_weapon(weapon):
 	var get_wep = weapon.instantiate() as Area2D
@@ -22,7 +22,11 @@ func _on_ami_projectle(pos, direction):
 		weapon.rotation_degrees = rad_to_deg(direction.angle()) + 90
 		weapon.direction = direction
 		$Projectiles.add_child(weapon)
+	elif Globals.wep_embrace == true:
+		var weapon = get_embrace_proj.instantiate() as Area2D
+		weapon.position = pos
+		$Projectiles.add_child(weapon)
 
 
-func _on_ui_weapon_selected() -> void:
-	Globals.game_ready = true
+#func _on_ui_weapon_selected() -> void:
+	#Globals.game_ready = true
