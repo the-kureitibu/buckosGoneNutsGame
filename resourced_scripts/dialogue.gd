a8cf207d3225caf0512a7a2e03419cf17fc78a47
Joshua Alcober <joshuaalcober2019@gmail.com>
2025-07-26 07:59:20 +0800

    Add war horn sfx. Finalize pre game dialogue's sprites

--- resourced_scripts/dialogue.gd
+++ resourced_scripts/dialogue.gd
@@ -15,17 +15,17 @@ extends Node2D
 var pre_game: Array[Dictionary] = [
 	{"speaker": "", "line": "", "emotion": ""},
 	{"speaker": "Narrator", "line": "Meanwhile...", "emotion": ""},
-	{"speaker": "Ami", "line": "Bucko, Bucko, Bucko, Bucko, Bucko, Bucko, Buckoo- Are?! ", "emotion": ""},
-	{"speaker": "Ami", "line": "Nani kore?", "emotion": ""},
+	{"speaker": "Ami", "line": "Bucko, Bucko, Bucko, Bucko, Bucko, Bucko, Buckoo- Are?! ", "emotion": "happy"},
+	{"speaker": "Ami", "line": "Nani kore?", "emotion": "monkas"},
 	{"speaker": "Elder1/Elder2", "line": "...", "emotion": ""},
 	{"speaker": "Elder3", "line": "You’ve come. Spidor Mama.", "emotion": ""},
 	{"speaker": "Elder3", "line": "We hast been neglected. As so it’s been decided we wage war.", "emotion": ""},
-	{"speaker": "Ami", "line": "Neglected??!", "emotion": ""},
+	{"speaker": "Ami", "line": "Neglected??!", "emotion": "annoyed"},
 	{"speaker": "Elder1", "line": "...", "emotion": ""},
 	{"speaker": "Elder2", "line": "...", "emotion": ""},
 	{"speaker": "Elder3", "line": "...", "emotion": ""},
-	{"speaker": "Ami", "line": "...", "emotion": ""},
-	{"speaker": "Ami", "line": "You guys gone nuts?! Fine! Come get some!!", "emotion": ""},
+	{"speaker": "Ami", "line": "...", "emotion": "annoyed"},
+	{"speaker": "Ami", "line": "You guys gone nuts?! Fine! Come get some!!", "emotion": "angry"},
 	{"speaker": "Elder3", "line": "Hah! Well quoth, Spidor. SOUND THE MARCH.", "emotion": ""}
 ]
 
@@ -42,7 +42,7 @@ var post_wave_one: Array[Dictionary] = [
 
 var pre_wave_two: Array[Dictionary] = [
 	{"speaker": "Elder2", "line": "Well done surviving until now, Spidor.", "emotion": ""},
-	{"speaker": "Ami", "line": "You tryna kill me or something?!?", "emotion": ""}
+	{"speaker": "Ami", "line": "You tryna kill me or something?!?", "emotion": "angry"}
 ]
 
 var post_wave_two: Array[Dictionary] = [
@@ -52,23 +52,23 @@ var post_wave_two: Array[Dictionary] = [
 
 var pre_wave_three: Array[Dictionary] = [
 	{"speaker": "Elder3", "line": "t is timeth, Spidor.", "emotion": ""},
-	{"speaker": "Ami", "line": "Show me what you got bucko.", "emotion": ""}
+	{"speaker": "Ami", "line": "Show me what you got bucko.", "emotion": "angry"}
 ]
 
 
 #ENDINGS 
 
 var true_end: Array[Dictionary] = [
-	{"speaker": "Ami", "line": "You spoiled brats all you want are chus!! You silly buckos! ", "emotion": ""},
+	{"speaker": "Ami", "line": "You spoiled brats all you want are chus!! You silly buckos! ", "emotion": "angry"},
 	{"speaker": "Elder3", "line": "It is not our fault  'tis gone from sight.", "emotion": ""}
 ]
 
 var bad_end: Array[Dictionary] = [
 	{"speaker": "Elder3", "line": "Thou've done it now, spidor mama.  I might not but admit our methods are rough,  yet  it is for the sake of getting pampered.  This is the end.  We bid thou farewell", "emotion": ""},
-	{"speaker": "Ami", "line": "No buckos… NOOOOOOOO!!!!", "emotion": ""}
+	{"speaker": "Ami", "line": "No buckos… NOOOOOOOO!!!!", "emotion": "annoyed"}
 ]
 
 var good_end: Array[Dictionary] = [
-	{"speaker": "Ami", "line": "Hmmp!", "emotion": ""},
+	{"speaker": "Ami", "line": "Hmmp!", "emotion": "angry"},
 	{"speaker": "Elder1/Elder2/Elder3", "line": "This situation isn’t ill at all.", "emotion": ""}
 ]
--- ui_scripts/pre_and_post_dialogue.gd
+++ ui_scripts/pre_and_post_dialogue.gd
@@ -3,6 +3,11 @@ extends Control
 @export var speaker_name: Label
 @export var dialogue_label: Label
 @export var hbox_cont: HBoxContainer
+@export var elder1: Sprite2D
+@export var elder2: Sprite2D
+@export var elder3: Sprite2D
+@onready var audio = $AudioStreamPlayer
+@export var ami: Sprite2D
 
 var current_index := 0
 var dialogue: Array
@@ -23,9 +28,75 @@ func _set_current_index(index: int):
 	dialogue_label.text = dialogue[current_index].line
 
 	if idx["speaker"] == "Ami":
+		ami.visible = true
 		hbox_cont.alignment = BoxContainer.ALIGNMENT_BEGIN
 	else:
 		hbox_cont.alignment = BoxContainer.ALIGNMENT_END
+	
+	if idx["speaker"] == "Ami" and idx["emotion"] == "happy":
+		$AnimationPlayer.play("ami_happy")
+	if idx["speaker"] == "Ami" and idx["emotion"] == "monkas":
+		ami.position.x = 68.0
+		$AnimationPlayer.play("ami_monkas")
+	if idx["speaker"] == "Ami" and idx["emotion"] == "angry":
+		ami.position.x = 65.0
+		$AnimationPlayer.play("ami_angry")
+	if idx["speaker"] == "Ami" and idx["emotion"] == "annoyed":
+		ami.position.x = 70.0
+		$AnimationPlayer.play("ami_annoyed")
+	
+	
+	if idx["speaker"] == "Elder1":
+		elder1.visible = true
+		elder2.visible = false
+		elder3.visible = false
+		elder1.position.x = 548.5
+		if idx.line == "...":
+			$AnimationPlayer.play("elder1_1")
+		else:
+			$AnimationPlayer.play("elder1")
+		
+	elif idx["speaker"] == "Elder2":
+		elder1.visible = false
+		elder2.visible = true
+		elder3.visible = false
+		elder2.position.x = 548.5
+		if idx.line == "...":
+			$AnimationPlayer.play("elder2_1")
+		else:
+			$AnimationPlayer.play("elder2")
+
+	elif idx["speaker"] == "Elder3":
+		elder1.visible = false
+		elder2.visible = false
+		elder3.visible = true
+		if idx.line == "...":
+			$AnimationPlayer.play("elder3_1")
+		else:
+			$AnimationPlayer.play("elder3")
+			
+	elif idx["speaker"] == "Elder1/Elder2":
+		elder1.visible = true
+		elder2.visible = true
+		elder3.visible = true
+		elder1.position.x = 400.0
+		elder2.position.x = 460.0
+		if idx.line == "...":
+			$AnimationPlayer.play("elder1_1")
+			$AnimationPlayer.play("elder2_1")
+		else:
+			$AnimationPlayer.play("elder1")
+			$AnimationPlayer.play("elder2")
+		
+	elif idx["speaker"] == "Elder1/Elder2/Elder3":
+		elder1.visible = true
+		elder2.visible = true
+		elder3.visible = true
+		elder1.position.x = 423.0
+		elder2.position.x = 482.0
+		#$AnimationPlayer.play("elder_one_1")
+		#$AnimationPlayer.play("elder_two_2")
+		#$AnimationPlayer.play("elder_3_one")
 
 
 func _unhandled_input(_event: InputEvent) -> void:
@@ -33,39 +104,31 @@ func _unhandled_input(_event: InputEvent) -> void:
 		current_index += 1
 		if current_index < dialogue.size():
 			_set_current_index(current_index)
+		elif current_index == dialogue.size():
+			if !audio.playing:
+				audio.play()
+			else:
+				if audio.playing:
+					audio.stop()
 		else:
 			speaker_name.text = ""
 			dialogue_label.text = ""
 			current_index = 0
+			ami.visible = false
+			elder1.visible = false 
+			elder2.visible = false
+			elder3.visible = false
 			await TransitionsManager.fade_in()
 			get_tree().change_scene_to_file("res://scenes/level_parent.tscn")
 			queue_free()
-
-#func set_current_dialogue(wave: int, part: int):
-	#match [wave, part]:
-		#[1, 1]:
-			#dialogue = Dialogue.pre_wave_one
-		#[1, 2]:
-			#dialogue = Dialogue.post_wave_one
-		#[2, 1]:
-			#dialogue = Dialogue.pre_wave_two
-		#[2, 1]:
-			#dialogue = Dialogue.post_wave_two
-		#[3, 1]:
-			#dialogue = Dialogue.pre_wave_three
-		#_:
-			#dialogue = []
-		#
-	#current_index = 0
-	#if dialogue.size() > 0:
-		#_set_current_index(current_index)
-		##run_wave_dialogue()
-	#else:
-		#push_error("Dialogue array empty for wave %d part %d" % [wave, part])
-#
-
+	
+	
 func _ready() -> void:
 	await TransitionsManager.fade_out()
+	ami.visible = false
+	elder1.visible = false 
+	elder2.visible = false
+	elder3.visible = false
 	
 	if !GameManager.game_started:
 		dialogue = Dialogue.pre_game
